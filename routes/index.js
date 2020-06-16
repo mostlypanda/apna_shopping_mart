38ab3c370dc59a634cf2a5cd0bcaec8fcd832b14
mostlypanda <sarthakmittal1461@gmail.com>
2020-06-16 19:31:32 +0530

    basic script added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Apna_Shopping_Mart' });
 });
 
 module.exports = router;
